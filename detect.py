8d89ed40196b64a492c90f724c250d957497f810
Zev Minsky-Primus <zevminskyprimus@gmail.com>
2024-01-10 16:34:59 -0500

    bloop

--- detect.py
+++ detect.py
@@ -1,6 +1,6 @@
 import keras_ocr # type: ignore 
 
-pipeline = keras_ocr.pipeline.Pipeline()
+pipeline = keras_ocr.pipeline.Pipeline(max_size=4096)
 
 relevant_words = {"copper, iron", "lead", "brass", "material"}
 
@@ -15,10 +15,12 @@ def relevant_files(files: list[str]) -> list[str]:
 
     results: list[str] = []
     for file, group in zip(files, prediction_groups):
-        all_words: set[str] = set()
+        all_words: list[str] = []
         for t in group:
-            all_words.add(t[0].lower())
-        if any(word in all_words for word in relevant_words):
+            all_words.append(t[0].lower())
+        print(f"{all_words = }")
+        all_words_set = set(all_words)
+        if any(word in all_words_set for word in relevant_words):
             results.append(file)
 
     return results
